d01c18c280d07ff99f6998bdf0507fd33a6a5b06
Shayne C <shaynec.0205@gmail.com>
2024-04-21 23:10:34 +0800

    feat: add google map in bridge detail page

--- lib/src/bridges/presentation/views/bridge_detail_page.dart
+++ lib/src/bridges/presentation/views/bridge_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:buy_and_ship_task/core/style/style.dart';
 import 'package:buy_and_ship_task/src/bridges/domain/entities/bridge.dart';
 import 'package:buy_and_ship_task/src/shared_widgets/scaffold_body_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 @RoutePage()
 class BridgeDetailPage extends StatelessWidget {
@@ -12,8 +13,7 @@ class BridgeDetailPage extends StatelessWidget {
   }) : super(key: key);
   final Bridge bridge;
 
-  @override
-  Widget build(BuildContext context) {
+  List<Widget> bridgeInfoTable(BuildContext context) {
     Map<String, dynamic> data = {
       '名稱': bridge.name,
       '主管機關': bridge.adm,
@@ -23,44 +23,72 @@ class BridgeDetailPage extends StatelessWidget {
       '經度': bridge.objLongitude,
       '緯度': bridge.objLatitude
     };
+    return data.entries
+        .map(
+          (e) => Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: ColorSystem.grey100),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 100.0,
+                    child: Text(
+                      e.key,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      '${e.value}',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+        .toList();
+  }
+
+  Widget mapView(BuildContext context) {
+    var latLng = LatLng(bridge.objLatitude, bridge.objLongitude);
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 3,
+      child: GoogleMap(
+        liteModeEnabled: true,
+        initialCameraPosition: CameraPosition(
+          zoom: 18,
+          target: latLng,
+        ),
+        markers: {
+          Marker(
+            markerId: MarkerId('${bridge.id}'),
+            position: latLng,
+          )
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(bridge.name),
       ),
       body: ScaffoldBodyLayout(
         child: Column(
-          children: data.entries
-              .map(
-                (e) => Container(
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: ColorSystem.grey100),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 100.0,
-                          child: Text(
-                            e.key,
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            '${e.value}',
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-              .toList(),
+          children: [
+            ...bridgeInfoTable(context),
+            mapView(context),
+          ],
         ),
       ),
     );
